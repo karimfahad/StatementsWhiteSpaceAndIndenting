360f385e6a5cb668c30b63509431963eda6e5cca
Fahad Karim <karimfahad@yahoo.com>
2018-05-30 00:05:18 -0400

    initial commit

---
+++ src/com/StatementsWhiteSpaceAndIndenting/Main.java
@@ -0,0 +1,15 @@
+package com.StatementsWhiteSpaceAndIndenting;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int myVariable=50;   //entire line is a statement
+		myVariable++;
+		myVariable--;
+		
+		int anotherVariable = 50;  myVariable--; //two statement can be on the same line
+		//whitespace is space between variable and operators
+		//indenting is code formating by java
+	}
+
+}
